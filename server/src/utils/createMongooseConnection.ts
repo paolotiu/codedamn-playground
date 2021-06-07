831aed8b00dcd0fefa222e8e4abd5d83b859d327
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-07 22:36:55 +0800

    added logs in dev

--- server/src/utils/createMongooseConnection.ts
+++ server/src/utils/createMongooseConnection.ts
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 export const createMongooseConnection = (url?: string) => {
+  // Add logs in dev
+  if (process.env.NODE_ENV === 'development') {
+    mongoose.set('debug', true);
+  }
   return mongoose.connect(url || (process.env.DB_URL as string), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
