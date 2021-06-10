6a45c9287c5aa2cd1a3692cc81c14c318e4521d1
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-06-10 17:55:58 +0800

    changed html boilerplate

--- server/src/utils/createDefaultFiles.ts
+++ server/src/utils/createDefaultFiles.ts
@@ -1,5 +1,51 @@
 import File from '@models/File';
 
+const htmlDefaultValue = `
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title> HTML Playground </title>
+
+  <link rel="stylesheet" href="/style.css">
+
+</head>
+
+<body>
+  <h1>Hello There!</h1>
+  <p>Have a nice day :)</p>
+
+<p>
+  It is currently:
+  <span id="time"></span>
+</p>
+    
+  <script src="/script.js"></script>
+</body>
+</html>
+
+`;
+
+const cssDefaultValue = `
+body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+#time{
+    color: darkblue
+}
+`;
+const jsDefaultValue = `
+const timer = document.querySelector('#time')
+
+setInterval(() => {
+	timer.innerText = new Date().toLocaleString()
+}, 1000)
+
+`;
+
 export const createDefaultFiles = async (
   userId: string,
   playgroundId: string
@@ -11,18 +57,18 @@ export const createDefaultFiles = async (
 
   const html = new File({
     name: 'index.html',
-    value: `<div> Hello World </div>`,
+    value: htmlDefaultValue,
     ...defaultFields,
   });
   const js = new File({
     name: 'script.js',
-    value: ``,
+    value: jsDefaultValue,
     ...defaultFields,
   });
 
   const css = new File({
     name: 'style.css',
-    value: '',
+    value: cssDefaultValue,
     ...defaultFields,
   });
 
